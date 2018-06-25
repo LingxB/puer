45da4fed1874948ecfe7e24222747a30c82c6e83
ebao <ebao@toolsgroup.com>
2018-06-25 23:25:05 +0200

    ckpt

--- src/scripts/sandbox.py
+++ src/scripts/sandbox.py
@@ -1,8 +1,47 @@
-from src.utils import Logger, __fn__
+from src.utils import Logger, __fn__, load_corpus, load_embedding
+from src.data import AbsaDataManager
+import numpy as np
+import tensorflow as tf
+from tensorflow.contrib import rnn
+from copy import deepcopy
+from time import time
 
 
 logger = Logger(__fn__())
 
-logger.info('info test')
 
-logger.debug('debug test')
+dm = AbsaDataManager()
+
+
+train_df = load_corpus('data/processed/ATAE-LSTM/train.csv')
+dev_df = load_corpus('data/processed/ATAE-LSTM/dev.csv')
+test_df = load_corpus('data/processed/ATAE-LSTM/test.csv')
+
+
+X = tf.placeholder(tf.int32, shape=(None, None), name='X')
+
+
+pre_trained_embedding = tf.get_variable(name="pre_trained_embedding",
+                                        shape=dm.emb.shape,
+                                        dtype=tf.float32,
+                                        initializer=tf.constant_initializer(dm.emb.values),
+                                        trainable=True)
+
+pad_embedding = tf.get_variable(name='pad_embedding',
+                                shape=(2, 300),
+                                dtype=tf.float32,
+                                initializer=tf.zeros_initializer(),
+                                trainable=False)
+
+embedding = tf.concat([pad_embedding, pre_trained_embedding], axis=0, name='concat_embedding')
+
+
+X_ = tf.nn.embedding_lookup(embedding, X)
+
+init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+    sess.run(init)
+    _, first_batch = next(dm.batch_generator(train_df, batch_size=5))
+    _X, _asp, _lx, _y = first_batch
+    lookup = sess.run(X_, feed_dict={X: _X})
