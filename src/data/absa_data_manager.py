db28eba7be5be463c27aaf89910abc90eee3e519
ebao <ebao@toolsgroup.com>
2018-06-25 17:54:12 +0200

    simplify dm

--- src/data/absa_data_manager.py
+++ src/data/absa_data_manager.py
@@ -12,35 +12,26 @@ X, asp, lex, y
 
 import pandas as pd
 import numpy as np
-from src.utils import load_corpus, symbolize, load_symbod_dict_if_exists, get_envar, read_config, create_dump_symbol_dict
+from src.utils import symbolize, get_envar, read_config, read_yaml
 from src.utils import Logger, __fn__
 pd.options.display.max_colwidth = 80
 logger = Logger(__fn__())
 
 class AbsaDataManager(object):
 
-    def __init__(self, dataset=None, lexicon_manager=None, x_col='SENT', y_col='CLS', asp_col='ASP'):
-        self.df = dataset
+    def __init__(self, lexicon_manager=None, x_col='SENT', y_col='CLS', asp_col='ASP'):
         self.x_col = x_col
         self.y_col = y_col
         self.asp_col = asp_col
         self.lm = lexicon_manager
-        self.config_path = get_envar('CONFIG_PATH')
-        self.configs = read_config(self.config_path+'/'+get_envar('BASE_CONFIG'), obj_view=True)
+        self.configs = read_config(get_envar('CONFIG_PATH')+'/'+get_envar('BASE_CONFIG'), obj_view=True)
         self.__initialize()
 
 
     def __initialize(self):
         sd_path = self.configs.symbol_dict.file_path
-        start_idx = self.configs.symbol_dict.start_idx
         logger.info('Loading symbol_dict from {}'.format(sd_path))
-        self.sd = load_symbod_dict_if_exists(sd_path)
-        if self.sd == False:
-            logger.info('symbol_dict not found, creating new on: {}, {}, start_idx: {}'.format(self.x_col, self.asp_col, start_idx))
-            self.corpus = pd.concat([self.df[self.x_col], pd.Series(self.df[self.asp_col].unique())], ignore_index=True).str.split()
-            logger.info('dataset shape: {}'.format(self.corpus.shape))
-            self.sd = create_dump_symbol_dict(self.corpus, start_idx, sd_path)
-            logger.info('symbod_dict saved to {}'.format(sd_path))
+        self.sd = read_yaml(sd_path)
 
     def token2symbol(self, sents):
         """
@@ -54,7 +45,7 @@ class AbsaDataManager(object):
         ``numpy.ndarray``
             Symbollized sentences padded with max length
         """
-        return symbolize(sents, self.sd)
+        return symbolize(sents, self.sd, unk=1)
 
 
     def class2symbol(self, labels):
@@ -88,6 +79,7 @@ class AbsaDataManager(object):
         return a
 
     def lex2symbol(self, sents):
+        # TODO: Implement
         pass
 
     def batch_symbolizer(self, batch_df):
@@ -151,48 +143,48 @@ class AbsaDataManager(object):
 #                      'data/processed/ATAE-LSTM/dev.csv',
 #                      'data/processed/ATAE-LSTM/test.csv'])
 
-train_df = load_corpus('data/processed/ATAE-LSTM/train.csv')
-
-train_df = train_df.head(50)
-
-
-dm = AbsaDataManager()
-
-
-gen = dm.batch_generator(train_df, 32, shuffle=True)
-
-_df, _sym = next(gen)
-
-
-_X = dm.token2symbol(_df.SENT.str.split())
-_a = dm.token2symbol(_df.ASP.str.split())
-_y = dm.class2symbol(_df.CLS.tolist()[0])
-
-
-
-
-
-
-
-
-
-
-batch1_y = train_df.CLS[:5]
-
-
-
-
-
-batch1_x = train_df.SENT[:5].str.split()
-batch2_x = train_df.SENT[5:10].str.split()
-
-dm.token2symbol(batch1_x)
-
-dm.token2symbol(batch2_x)
-
-
-batch1_a = train_df.ASP[:5].str.split()
-dm.token2symbol(batch1_a)
+# train_df = load_corpus('data/processed/ATAE-LSTM/train.csv')
+#
+# train_df = train_df.head(50)
+#
+#
+# dm = AbsaDataManager()
+#
+#
+# gen = dm.batch_generator(train_df, 32, shuffle=True)
+#
+# _df, _sym = next(gen)
+#
+#
+# _X = dm.token2symbol(_df.SENT.str.split())
+# _a = dm.token2symbol(_df.ASP.str.split())
+# _y = dm.class2symbol(_df.CLS.tolist()[0])
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# batch1_y = train_df.CLS[:5]
+#
+#
+#
+#
+#
+# batch1_x = train_df.SENT[:5].str.split()
+# batch2_x = train_df.SENT[5:10].str.split()
+#
+# dm.token2symbol(batch1_x)
+#
+# dm.token2symbol(batch2_x)
+#
+#
+# batch1_a = train_df.ASP[:5].str.split()
+# dm.token2symbol(batch1_a)
 
 
 
