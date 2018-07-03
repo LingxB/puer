f18b9662ff92f8884cfa61bcd7ae1be7762bfc80
ebao <ebao@toolsgroup.com>
2018-07-03 14:41:49 +0200

    ckpt

--- src/scripts/sandbox.py
+++ src/scripts/sandbox.py
@@ -1,4 +1,4 @@
-from src.utils import Logger, __fn__, load_corpus, load_embedding
+from src.utils import Logger, __fn__, load_corpus, matmul_2_3, seq_length
 from src.data import AbsaDataManager
 import numpy as np
 import tensorflow as tf
@@ -9,16 +9,12 @@ from time import time
 
 logger = Logger(__fn__())
 
-def length(sequence):
-  used = tf.sign(tf.reduce_max(tf.abs(sequence), 2))
-  length = tf.reduce_sum(used, 1)
-  length = tf.cast(length, tf.int32)
-  return length
 
 # Hyper parameters
+# ----------------
 hyparams = dict(
     random_state=None, # TODO: TEST FIXED RANDOM SEED
-    batch_size=25,
+    batch_size=5,
     cell_num=300,
     layer_num=1,
     dropout_keep_prob=0.5,
@@ -28,57 +24,93 @@ hyparams = dict(
     lambta = 0.01  # L2
 )
 
+
 # Load data
+# ---------
 dm = AbsaDataManager()
 
 train_df = load_corpus('data/processed/ATAE-LSTM/train.csv')
 dev_df = load_corpus('data/processed/ATAE-LSTM/dev.csv')
 test_df = load_corpus('data/processed/ATAE-LSTM/test.csv')
 
+
 # Input place holders
+# -------------------
 X = tf.placeholder(tf.int32, shape=(None, None), name='X')
 asp = tf.placeholder(tf.int32, shape=(None, 1), name='asp')
 y = tf.placeholder(tf.int32, shape=(None, 3), name='y')
 
-dropout = tf.placeholder(tf.float32, shape=(1, ), name='dropout_keep')
+dropout_keep = tf.placeholder(tf.float32, shape=(1, ), name='dropout_keep')
 
 # TODO: IMPLEMENT LX
 # lx = tf.placeholder(tf.int32, shape=(None, None), name='lx')
 
+
+# Initializer
+# -----------
+initializer = tf.random_uniform_initializer(minval=-hyparams['epsilon'],
+                                            maxval=hyparams['epsilon'],
+                                            seed=hyparams['random_state'])
+
 # Embedding
+# ---------
 with tf.name_scope('Embedding'):
     glove = tf.get_variable(name='glove',
                             shape=dm.emb.shape,
                             dtype=tf.float32,
                             initializer=tf.constant_initializer(dm.emb.values),
-                            trainable=True)
+                            )
     pad = tf.get_variable(name='pad',
-                          shape=(1, glove.shape[1].value),
+                          shape=(1, glove.shape[1]),
                           dtype=tf.float32,
                           initializer=tf.zeros_initializer(),
                           trainable=False)
     unk = tf.get_variable(name='unk',
-                          shape=(1, glove.shape[1].value),
+                          shape=(1, glove.shape[1]),
                           dtype=tf.float32,
-                          initializer=tf.random_uniform_initializer(minval=-hyparams['epsilon'],
-                                                                    maxval=hyparams['epsilon'],
-                                                                    seed=hyparams['random_state']),
-                          trainable=True)
+                          initializer=initializer,
+                          )
     embedding = tf.concat([pad, unk, glove], axis=0, name='embedding')
 
-    X_ = tf.nn.embedding_lookup(embedding, X) # (batch, max_len, embedding_size)
-    seq_len = length(X_)
-    asp_ = tf.nn.embedding_lookup(embedding, asp) # (batch, 1, embedding_size)
+    X_ = tf.nn.embedding_lookup(embedding, X) # (batch, N, d)
+    seq_len = seq_length(X_)
+    asp_ = tf.nn.embedding_lookup(embedding, asp) # (batch, 1, d)
 
 
+# Encoder
+# -------
 with tf.name_scope('Encoder'):
     cell = rnn.BasicLSTMCell(hyparams['cell_num'])
     # cell = rnn.LSTMCell(hyparams['cell_num'])
     cells = [deepcopy(cell) for i in range(hyparams['layer_num'])]
     cell = rnn.MultiRNNCell(cells)
 
-    H, (s,) = tf.nn.dynamic_rnn(cell=cell, inputs=X_, sequence_length=seq_len, dtype=tf.float32)
+    H, (s,) = tf.nn.dynamic_rnn(cell=cell, inputs=X_, sequence_length=seq_len, dtype=tf.float32) # (batch, N, d)
+    assert H.shape.as_list() == tf.TensorShape([X_.shape[0], X_.shape[1], hyparams['cell_num']]).as_list()
 
+
+# Attention
+# ---------
+H_T = tf.transpose(H, [0, 2, 1]) # (batch, d, N)
+Wh = tf.get_variable('Wh', shape=(hyparams['cell_num'], hyparams['cell_num']), dtype=tf.float32, initializer=initializer) # (d, d)
+WhH = matmul_2_3(Wh, H_T) # (batch, d, N)
+
+VaeN = tf.tile(asp_, [1, tf.shape(X_)[1], 1]) # (batch, N, da), da==d in this setting
+assert VaeN.shape.as_list() == X_.shape.as_list()
+VaeN_T = tf.transpose(VaeN, [0, 2, 1]) # (batch, da, N)
+Wv = tf.get_variable('Wv', shape=(asp_.shape[2], asp_.shape[2]), dtype=tf.float32, initializer=initializer) # (da, da)
+WaVaeN = matmul_2_3(Wv, VaeN_T) # (batch, da, N)
+
+
+
+
+# _H = tf.transpose(H, [2, 1, 0]) # (d, N, batch)
+# __H = tf.reshape(_H, (_H.shape[0], -1)) # (d, N*batch)
+#
+# Wh = tf.get_variable('Wa', shape=(glove.shape[1], glove.shape[1]), dtype=tf.float32)
+#
+# _WhH = tf.matmul(Wh, __H)
+# WhH = tf.reshape(_WhH, (_H.shape[0], -1, hyparams['batch_size']))
 
 
 
@@ -89,12 +121,17 @@ init = tf.global_variables_initializer()
 
 with tf.Session() as sess:
     sess.run(init)
-    _, first_batch = next(dm.batch_generator(train_df, batch_size=5))
+    _, first_batch = next(dm.batch_generator(train_df, batch_size=hyparams['batch_size']))
     _X, _asp, _lx, _y = first_batch
 
     #X_lookup, s_len, asp_lookup = sess.run([X_, seq_len, asp_], feed_dict={X: _X, asp: _asp})
 
-    H, s = sess.run([H, s], feed_dict={X: _X, asp: _asp})
+    # H, s, _H = sess.run([H, s, _H], feed_dict={X: _X, asp: _asp})
 
 
-
+    H, Wh, WhH = sess.run([H, Wh, WhH], feed_dict={X: _X, asp: _asp})
+#
+#
+#
+# _H = H.transpose([2, 1, 0])
+# __H = _H.reshape(300, -1)
