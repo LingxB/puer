38108c0306ded7d76dddeb7b805fb8ccd286a1e4
ebao <ebao@toolsgroup.com>
2018-07-03 22:31:27 +0200

    ckpt

--- src/scripts/sandbox.py
+++ src/scripts/sandbox.py
@@ -15,7 +15,7 @@ logger = Logger(__fn__())
 hyparams = dict(
     random_state=None, # TODO: TEST FIXED RANDOM SEED
     batch_size=5,
-    cell_num=300,
+    cell_num=300, # d
     layer_num=1,
     dropout_keep_prob=0.5,
     epsilon=0.01,
@@ -40,8 +40,7 @@ X = tf.placeholder(tf.int32, shape=(None, None), name='X')
 asp = tf.placeholder(tf.int32, shape=(None, 1), name='asp')
 y = tf.placeholder(tf.int32, shape=(None, 3), name='y')
 
-dropout_keep = tf.placeholder(tf.float32, shape=(1, ), name='dropout_keep')
-
+dropout_keep = tf.placeholder_with_default(1.0, shape=(), name='dropout_keep')
 # TODO: IMPLEMENT LX
 # lx = tf.placeholder(tf.int32, shape=(None, None), name='lx')
 
@@ -74,44 +73,77 @@ with tf.name_scope('Embedding'):
 
     X_ = tf.nn.embedding_lookup(embedding, X) # (batch, N, d)
     seq_len = seq_length(X_)
-    asp_ = tf.nn.embedding_lookup(embedding, asp) # (batch, 1, d)
+    asp_ = tf.nn.embedding_lookup(embedding, asp) # (batch, 1, da)
 
 
 # Encoder
 # -------
 with tf.name_scope('Encoder'):
-    cell = rnn.BasicLSTMCell(hyparams['cell_num'])
-    # cell = rnn.LSTMCell(hyparams['cell_num'])
+    # cell = rnn.BasicLSTMCell(hyparams['cell_num'])
+    cell = rnn.LSTMCell(hyparams['cell_num'], initializer=initializer)
     cells = [deepcopy(cell) for i in range(hyparams['layer_num'])]
     cell = rnn.MultiRNNCell(cells)
 
     H, (s,) = tf.nn.dynamic_rnn(cell=cell, inputs=X_, sequence_length=seq_len, dtype=tf.float32) # (batch, N, d)
+    hN = s.h # (batch, d)
     assert H.shape.as_list() == tf.TensorShape([X_.shape[0], X_.shape[1], hyparams['cell_num']]).as_list()
 
 
 # Attention
 # ---------
-H_T = tf.transpose(H, [0, 2, 1]) # (batch, d, N)
-Wh = tf.get_variable('Wh', shape=(hyparams['cell_num'], hyparams['cell_num']), dtype=tf.float32, initializer=initializer) # (d, d)
-WhH = matmul_2_3(Wh, H_T) # (batch, d, N)
+with tf.name_scope('Attention'):
+    H_T = tf.transpose(H, [0, 2, 1]) # (batch, d, N)
+    Wh = tf.get_variable('Wh', shape=(hyparams['cell_num'], hyparams['cell_num']), dtype=tf.float32, initializer=initializer) # (d, d)
+    WhH = matmul_2_3(Wh, H_T) # (batch, d, N)
+    assert WhH.shape.as_list() == H_T.shape.as_list()
 
-VaeN = tf.tile(asp_, [1, tf.shape(X_)[1], 1]) # (batch, N, da), da==d in this setting
-assert VaeN.shape.as_list() == X_.shape.as_list()
-VaeN_T = tf.transpose(VaeN, [0, 2, 1]) # (batch, da, N)
-Wv = tf.get_variable('Wv', shape=(asp_.shape[2], asp_.shape[2]), dtype=tf.float32, initializer=initializer) # (da, da)
-WaVaeN = matmul_2_3(Wv, VaeN_T) # (batch, da, N)
+    VaeN = tf.tile(asp_, [1, tf.shape(X_)[1], 1]) # (batch, N, da), da==d in this setting
+    assert VaeN.shape.as_list() == X_.shape.as_list()
+    VaeN_T = tf.transpose(VaeN, [0, 2, 1]) # (batch, da, N)
+    Wv = tf.get_variable('Wv', shape=(asp_.shape[2], asp_.shape[2]), dtype=tf.float32, initializer=initializer) # (da, da)
+    WvVaeN = matmul_2_3(Wv, VaeN_T) # (batch, da, N)
+    assert WvVaeN.shape.as_list() == VaeN_T.shape.as_list()
+
+    M = tf.tanh(tf.concat([WhH, WvVaeN], axis=1)) # (batch, d+da, N)
+
+    w = tf.get_variable('w', shape=(X_.shape[2]+asp_.shape[2], 1), dtype=tf.float32, initializer=initializer) # (d+da, 1)
+    w_T = tf.transpose(w) # (1, d+da)
+    alpha = tf.nn.softmax(matmul_2_3(w_T, M)) # (batch, 1, N)
+    assert alpha.shape.as_list() == tf.TensorShape([X_.shape[0], 1, M.shape[2]]).as_list()
+    #alpha = tf.reshape(alpha, (tf.shape(alpha)[0], tf.shape(alpha)[2])) # (batch, N)
+
+    _r = tf.matmul(alpha, H) # (batch, 1, d)
+    r = tf.squeeze(_r, 1) # (batch, d)
+    assert r.shape.as_list() == tf.TensorShape([X_.shape[0], H.shape[2]]).as_list()
+
+    Wp = tf.get_variable('Wp', shape=(r.shape[1], r.shape[1]), dtype=tf.float32, initializer=initializer) # (d, d)
+    Wx = tf.get_variable('Wx', shape=(hN.shape[1], hN.shape[1]), dtype=tf.float32, initializer=initializer) # (d, d)
+    h_star = tf.tanh(tf.matmul(r, Wp) + tf.matmul(hN, Wx))
+    h_star = tf.nn.dropout(h_star, dropout_keep) # 0.5 dropout on h_star was found in author's code
+    assert h_star.shape.as_list() == tf.TensorShape([H.shape[0], H.shape[2]]).as_list()
 
 
+# Output Layer
+# ------------
+with tf.name_scope('Output'):
+    logits = tf.layers.dense(h_star, 3, kernel_initializer=initializer, name='s')
+    pred = tf.nn.softmax(logits)
 
 
-# _H = tf.transpose(H, [2, 1, 0]) # (d, N, batch)
-# __H = tf.reshape(_H, (_H.shape[0], -1)) # (d, N*batch)
-#
-# Wh = tf.get_variable('Wa', shape=(glove.shape[1], glove.shape[1]), dtype=tf.float32)
-#
-# _WhH = tf.matmul(Wh, __H)
-# WhH = tf.reshape(_WhH, (_H.shape[0], -1, hyparams['batch_size']))
+# Loss
+# ----
+# TODO: Check loss, use reduce_sum instead of reduce_mean correct?
+# TODO: Check L2, current implenmentation loss is not normalized by batch_size
+cross_entropy = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits_v2(labels=y, logits=logits))
 
+regularizer = hyparams['lambta'] * tf.add_n([tf.nn.l2_loss(p) for p in tf.trainable_variables()])
+
+loss = cross_entropy + regularizer
+
+# Train Op
+# --------
+#train_op = tf.train.AdagradOptimizer(0.01).minimize(loss)
+train_op = tf.train.AdamOptimizer(learning_rate=0.01)
 
 
 init = tf.global_variables_initializer()
@@ -124,12 +156,14 @@ with tf.Session() as sess:
     _, first_batch = next(dm.batch_generator(train_df, batch_size=hyparams['batch_size']))
     _X, _asp, _lx, _y = first_batch
 
+    sess.run()
+
     #X_lookup, s_len, asp_lookup = sess.run([X_, seq_len, asp_], feed_dict={X: _X, asp: _asp})
 
     # H, s, _H = sess.run([H, s, _H], feed_dict={X: _X, asp: _asp})
 
 
-    H, Wh, WhH = sess.run([H, Wh, WhH], feed_dict={X: _X, asp: _asp})
+    # H, Wh, WhH = sess.run([H, Wh, WhH], feed_dict={X: _X, asp: _asp})
 #
 #
 #
