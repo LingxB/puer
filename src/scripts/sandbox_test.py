2861c1d4e8058b6701d1a4e30ffc0f2c70b9defe
ebao <ebao@toolsgroup.com>
2018-07-18 18:15:28 +0200

    fix tensor name

--- src/scripts/sandbox_test.py
+++ src/scripts/sandbox_test.py
@@ -17,8 +17,8 @@ test_df = load_corpus('data/processed/ATAE-LSTM/test.csv')
 
 sess = tf.Session()
 
-saver = tf.train.import_meta_graph('models/20180715_193759/AT-LSTM.meta')
-saver.restore(sess, tf.train.latest_checkpoint('models/20180715_193759'))
+saver = tf.train.import_meta_graph('models/20180716_221606/AT-LSTM.meta')
+saver.restore(sess, tf.train.latest_checkpoint('models/20180716_221606'))
 
 graph = tf.get_default_graph()
 all_ops = [op.name for op in graph.get_operations()]
@@ -28,15 +28,16 @@ X = graph.get_tensor_by_name('X:0')
 asp = graph.get_tensor_by_name('asp:0')
 y = graph.get_tensor_by_name('y:0')
 
-pred = graph.get_tensor_by_name('Output/Softmax:0')
-accuarcy = graph.get_tensor_by_name('Evaluation/Mean:0')
+alpha = graph.get_tensor_by_name('Attention/ALPHA:0')
+pred = graph.get_tensor_by_name('Output/PRED:0')
+accuarcy = graph.get_tensor_by_name('Evaluation/ACC3:0')
 
-batch_generator = dm.batch_generator(test_df, 25)
+batch_generator = dm.batch_generator(test_df, -1)
 
 _, batch = next(batch_generator)
 _X, _asp, _lx, _y = batch
 
-pred_, accuarcy_ = sess.run([pred, accuarcy], feed_dict={X: _X, asp: _asp, y: _y})
+alpha_, pred_, accuarcy_ = sess.run([alpha, pred, accuarcy], feed_dict={X: _X, asp: _asp, y: _y})
 
 
 sess.close()
