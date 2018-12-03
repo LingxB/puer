9fd38ac62542f8b3b7e896a44de81f7585a09bdd
ebao <ebao@toolsgroup.com>
2018-12-03 19:35:23 +0100

    Add/update error analysis for improvement cases

--- src/visualization/plots.py
+++ src/visualization/plots.py
@@ -13,7 +13,7 @@ def lx_get(df, key, default=0):
     except KeyError:
         return np.array(default)
 
-def plot_sent(tokens: list, alpha: str or list or np.ndarray, lexicon: pd.DataFrame):
+def plot_sent(tokens: list, alpha: str or list or np.ndarray, lexicon: pd.DataFrame, title=None, figsize=None):
     lx = np.array([lx_get(lexicon, w).mean() for w in tokens])
     if isinstance(alpha, str):
         attention = str2array(alpha)[:len(tokens)]
@@ -22,26 +22,28 @@ def plot_sent(tokens: list, alpha: str or list or np.ndarray, lexicon: pd.DataFr
     assert len(tokens) == len(attention)
     data = pd.DataFrame({'Lexicon': lx, 'Attention': attention})
 
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=figsize)
     ax.matshow(data.mask(((data == data) | data.isnull()) & (data.columns != "Attention")).transpose(), cmap=cm.viridis)
     ax.matshow(data.mask(((data == data) | data.isnull()) & (data.columns != "Lexicon")).transpose(), cmap=cm.cividis)
     ax.set_xticks(np.arange(len(tokens)))
     ax.set_xticklabels(tokens)
     ax.set_yticks(np.arange(2))
     ax.set_yticklabels(['Lexicon', 'Attention'])
+    if title:
+        ax.set_xlabel(title)
 
     for i in range(len(tokens)):
         for j in range(2):
             ax.text(i, j, round(data.iloc[i, j], 2), ha='center', va='center', color='w')
 
 
-# n = 331
+# n = 214
 #
 # df2 = pd.read_csv('data/score/SemEval14_test_baseline_v1.csv')
 # lexicon = pd.read_csv('data/processed/lexicon/lexicon_table.csv', index_col='WORD')
 # s2 = df2.iloc[n]
 # tokens = s2.SENT.split()
-# plot_sent(s2.SENT.split(), s2.ALPHA, lexicon)
+# plot_sent(s2.SENT.split(), s2.ALPHA, lexicon, title='Baseline')
 #
 # df = pd.read_csv('data/score/SemEval14_test_exp_3_v1.csv')
 # lexicon = pd.read_csv('data/processed/lexicon/lexicon_table.csv', index_col='WORD')
