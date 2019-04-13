f5b1ee58a73e4a671e42ba746db87cf7cbe38265
ebao <ebao@toolsgroup.com>
2019-04-13 18:00:00 +0200

    add att_std, fix att_ent

--- src/models/atlx.py
+++ src/models/atlx.py
@@ -183,7 +183,6 @@ class ATLX(BaseModel):
                                         tf.to_float(tf.shape(X_)[0]), name='REGL')
                 if self.p.get('att_reg'):
                     epsilon = self.p.get('epsilon')
-                    # TODO: Rewrite this part, not using, reg should be mean std in batch
                     # compute excluding padding positions
                     condition = tf.sequence_mask(seq_len, tf.reduce_max(seq_len))
                     a_true = tf.boolean_mask(tf.squeeze(alpha, 1), condition)
@@ -201,6 +200,11 @@ class ATLX(BaseModel):
                     a_true = tf.boolean_mask(tf.squeeze(alpha, 1), condition)
                     att_enh = -1 * gamma * tf.reduce_mean(a_true)
                     loss = tf.add_n([cross_entropy, regularizer, att_enh], name='LOSS')
+                elif self.p.get('att_std'):
+                    epsilon = self.p.get('epsilon')
+                    _, var = tf.nn.moments(tf.squeeze(alpha, 1), axes=1)
+                    att_std = epsilon * tf.reduce_mean(tf.sqrt(var))
+                    loss = tf.add_n([cross_entropy, regularizer, att_std], name='LOSS')
                 elif self.p.get('att_lmax'):
                     # + max
                     epsilon = self.p.get('epsilon')
@@ -208,10 +212,10 @@ class ATLX(BaseModel):
                     lmax = epsilon * tf.reduce_mean(tf.reduce_max(_alpha, axis=1))
                     loss = tf.add_n([cross_entropy, regularizer, lmax], name='LOSS')
                 elif self.p.get('att_ent'):
-                    # - entropy
+                    # - entropy want it to be bigger, more suprise, less sharp
                     epsilon = self.p.get('epsilon')
                     _alpha = tf.squeeze(alpha, 1)
-                    ent = epsilon * tf.reduce_mean(- tf.reduce_sum(_alpha * tf.log(_alpha), axis=1))
+                    ent = -1 * epsilon * tf.reduce_mean(- tf.reduce_sum(_alpha * tf.log(_alpha), axis=1))
                     loss = tf.add_n([cross_entropy, regularizer, ent], name='LOSS')
                 else:
                     loss = tf.add(cross_entropy, regularizer, name='LOSS')
